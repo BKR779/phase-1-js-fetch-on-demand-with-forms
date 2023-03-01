769c302770bff3429d2c35098845a86fcd585afc
Abobakr Mahdi <93738112+BKR779@users.noreply.github.com>
2023-03-02 00:56:11 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
 const init = () => {
-  
+  const inputForm = document.querySelector("form");
+  inputForm.addEventListener("submit", handleSubmit);
+};
+
+function handleSubmit(e) {
+  e.preventDefault();
+  // console.log(e.target.children.searchByID.value);
+  //e.target.children[1].value
+  const input = document.querySelector("input#searchByID");
+  console.log(input.value);
+
+  fetch(`http://localhost:3000/movies/${input.value}`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      const title = document.querySelector("section#movieDetails h4");
+      const summary = document.querySelector("section#movieDetails p");
+
+      title.innerText = data.title || `Title`;
+      summary.innerText = data.summary || `Summary`;
+      if (title.innerHTML === `Title`) alert("The Movie Doesn't Exist");
+    });
 }
 
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener("DOMContentLoaded", init);
